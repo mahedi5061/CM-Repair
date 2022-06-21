0e1dd27471a8e51a4adeaf8b4362608184c51822
mahedi5061 <mahedi5061@gmail.com>
2022-06-21 17:16:58 +0600

    authentication

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    }).save();
+    res.status(200).json(newUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const currentUser = await User.findOne({ email: req.body.email });
+    !currentUser && res.status(400).json("wrong email!");
+    const currentPassword = await bcrypt.compare(
+      req.body.password,
+      currentUser.password
+    );
+    !currentPassword && res.status(400).json("wrong password!");
+    const { password, ...other } = currentUser._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
